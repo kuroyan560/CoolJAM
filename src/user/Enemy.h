aaa86f1540ebd2800c05b4913be7ddf0bc3775f9
フナクラベ <k020g1164@g.neec.ac.jp>
2022-09-09 14:51:06 +0900

    敵の移動速度を更新。

--- src/user/Enemy.h
+++ src/user/Enemy.h
@@ -23,7 +23,7 @@ private:
 	float m_scale;				// 大きさ
 	float m_speed;				// 移動速度
 	const float SPEED = 0.6f;	// 移動速度のデフォルト値。
-	const float TRACKING_SPEED = 1.6f;
+	const float TRACKING_SPEED = 1.0f;
 	bool m_isActive;			// 生存フラグ
 	int m_hitEffectTimer;		// 弾に当たったフラグ
 	const int HIT_EFFECT_TIMER = 10;
