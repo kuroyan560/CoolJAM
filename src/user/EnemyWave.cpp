ac58ff8c685b889579906f98c3816d71789a436c
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-09-10 12:07:13 +0900

    Waveの切り替え処理を実装 そのステージがボーナスステージなのかどうか判定を返す処理を実装

--- src/user/EnemyWave.cpp
+++ src/user/EnemyWave.cpp
@@ -1,14 +1,16 @@
 #include "EnemyWave.h"
 #include "EnemyMgr.h"
 
-EnemyWave::EnemyWave(const int& WaveStartFrame)
+EnemyWave::EnemyWave(const int &WaveStartFrame, const bool &BounusStageFlag)
 {
 
 	/*===== コンストラクタ =====*/
 
 	m_waveStartFrame = WaveStartFrame;
 	m_nowWaveFrame = 0;
-
+	m_isBounusStageFlag = BounusStageFlag;
+	m_startWaveFlag = false;
+	m_initFlag = false;
 }
 
 void EnemyWave::Init()
@@ -17,17 +19,20 @@ void EnemyWave::Init()
 	/*===== 初期化処理 =====*/
 
 	m_nowWaveFrame = 0;
-
+	m_startWaveFlag = true;
+	m_initFlag = true;
 }
 
-void EnemyWave::Update(std::weak_ptr<EnemyMgr> EnemyMgr, const Vec3<float>& PlayerPos, const float& MapSize)
+void EnemyWave::Update(std::weak_ptr<EnemyMgr> EnemyMgr, const Vec3<float> &PlayerPos, const float &MapSize)
 {
-
 	/*===== 更新処理 ====*/
+	if (!m_startWaveFlag) {
+		return;
+	}
 
 	++m_nowWaveFrame;
 
-	for (auto& index : m_enemys) {
+	for (auto &index : m_enemys) {
 
 		// 生成フレームが合っていなかったら処理を飛ばす。
 		if (index.m_generateFrame != m_nowWaveFrame) continue;
@@ -47,7 +52,7 @@ void EnemyWave::Update(std::weak_ptr<EnemyMgr> EnemyMgr, const Vec3<float>& Play
 
 }
 
-void EnemyWave::AddEnemy(const ENEMY_INFO::SPAWN_INFO& EnemyInfo)
+void EnemyWave::AddEnemy(const ENEMY_INFO::SPAWN_INFO &EnemyInfo)
 {
 
 	/*===== 敵を追加 =====*/
@@ -56,7 +61,7 @@ void EnemyWave::AddEnemy(const ENEMY_INFO::SPAWN_INFO& EnemyInfo)
 
 }
 
-void EnemyWave::AddEnemy(const Vec3<float>& Pos, const Vec3<float>& ForwardVec, ENEMY_INFO::ID ID, const int& GenerateFrame)
+void EnemyWave::AddEnemy(const Vec3<float> &Pos, const Vec3<float> &ForwardVec, ENEMY_INFO::ID ID, const int &GenerateFrame)
 {
 
 	/*===== 敵を追加 =====*/
@@ -70,4 +75,24 @@ void EnemyWave::AddEnemy(const Vec3<float>& Pos, const Vec3<float>& ForwardVec, 
 
 	m_enemys.emplace_back(enemyInfo);
 
+}
+
+bool EnemyWave::IsBounusStage()
+{
+	return m_isBounusStageFlag;
+}
+
+int EnemyWave::WaveStartTime()
+{
+	return m_waveStartFrame;
+}
+
+void EnemyWave::Stop()
+{
+	m_startWaveFlag = false;
+}
+
+bool EnemyWave::IsStart()
+{
+	return m_initFlag;
 }
--- src/user/EnemyWave.h
+++ src/user/EnemyWave.h
@@ -46,16 +46,21 @@ private:
 	std::vector<ENEMY_INFO::SPAWN_INFO> m_enemys;	// 敵データ
 	int m_waveStartFrame;							// ウェーブ開始フレーム
 	int m_nowWaveFrame;								// このWaveが始まってからの経過時間。
-
-
+	bool m_isBounusStageFlag;
+	bool m_startWaveFlag;
+	bool m_initFlag;
 public:
 
 	/*===== メンバ関数 =====*/
 
-	EnemyWave(const int& WaveStartFrame);
+	EnemyWave(const int &WaveStartFrame, const bool &BounusStageFlag);
 	void Init();
 	void Update(std::weak_ptr<EnemyMgr> EnemyMgr, const Vec3<float>& PlayerPos, const float& MapSize);
 	void AddEnemy(const ENEMY_INFO::SPAWN_INFO& EnemyInfo);
 	void AddEnemy(const Vec3<float>& Pos, const Vec3<float>& ForwardVec, ENEMY_INFO::ID ID, const int& GenerateFrame);
 
+	bool IsStart();
+	void Stop();
+	bool IsBounusStage();
+	int WaveStartTime();
 };
