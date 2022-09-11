5ccb1a65e29dd76414ee06340143a706427761ff
hassy560 <k020g1167@g.neec.ac.jp>
2022-09-11 16:09:17 +0900

    カラーパレットのオレンジをピンクに変更

--- src/user/ColorPalette.h
+++ src/user/ColorPalette.h
@@ -4,7 +4,7 @@
 namespace ColorPalette
 {
 	static const Color S_GREEN_COLOR(31, 245, 147, 255);
-	static const Color S_ORANGE_COLOR(245, 93, 31, 255);
+	static const Color S_PINK_COLOR(0.88f, 0.012f, 0.658f, 1.0f);
 	static const Color S_LIGHT_PURPLE_COLOR(66, 28, 119, 255);
 	static const Color S_DARK_PURPLE_COLOR(23, 14, 41, 255);
 }
