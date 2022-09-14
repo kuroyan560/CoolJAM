ada8cee8b22b7873148f0e876b5f664c9d8589a2
フナクラベ <k020g1164@g.neec.ac.jp>
2022-09-14 16:46:34 +0900

    フィーバーゲージを実装。

--- src/user/FeverGauge.h
+++ src/user/FeverGauge.h
@@ -12,15 +12,17 @@ private:
 	/*====== メンバ変数 =====*/
 
 	std::shared_ptr<TextureBuffer> m_gaugeTexture;
+	std::array<std::shared_ptr<TextureBuffer>, 6> m_feverText;
 	float m_baseRate;
 	float m_nowRate;
 	const float GAUGE_LENGTH = 300.0f;
 	bool m_isActive;
 
-	std::array<float, 5> m_sinWave;	// サイン波に使用する
+	std::array<float, 6> m_sinWave;	// サイン波に使用する
 	const float ADD_SIN_WAVE = 0.2f;
-	const float SIN_WAVE_LENGTH = 20.0f;
+	const float SIN_WAVE_LENGTH = 30.0f;
 	float m_alpha;					// アルファ値
+	float m_lissajousTimer;			// リサージュ曲線用タイマー
 
 
 public:
