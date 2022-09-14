c669764d08c0dc94a5f79120f916a74af7f4fa2d
LC1A_07_オオイシカズノリ <k020g1151@g.neec.ac.jp>
2022-09-14 11:22:50 +0900

    BonusStageのフォントの登場処理を実装

--- src/user/BonusEffect.cpp
+++ src/user/BonusEffect.cpp
@@ -4,6 +4,7 @@
 BonusEffect::BonusEffect() :LIMIT_MIN_LINE(-1000.0f), LIMIT_MAX_LINE(1580.0f), APPEAR_TIME(60)
 {
 	m_feverEffectTexBuffer = D3D12App::Instance()->GenerateTextureBuffer("resource/user/BonusStage/BonusEffect.png");
+	m_bonusStageTexBuffer = D3D12App::Instance()->GenerateTextureBuffer("resource/user/BonusStage/bonus_stage.png");
 	m_startFlag = false;
 	m_stopFlag = false;
 
@@ -11,6 +12,12 @@ BonusEffect::BonusEffect() :LIMIT_MIN_LINE(-1000.0f), LIMIT_MAX_LINE(1580.0f), A
 	m_pos = { 500.0f,LIMIT_MIN_LINE };
 	m_vel.y = (LIMIT_MAX_LINE + abs(LIMIT_MIN_LINE)) / static_cast<float>(APPEAR_TIME);
 
+	appearDistance = -1250.0f;
+
+	appearRateData.Init({ 0.0f, LIMIT_MAX_LINE });
+	disappearRateData.Init({ 0.0f, LIMIT_MAX_LINE + appearDistance });
+	m_bigFontTimer = 0;
+	m_fontSize = { 1.0f,1.0f };
 }
 
 void BonusEffect::Init()
@@ -24,6 +31,35 @@ void BonusEffect::Update()
 		return;
 	}
 
+	if (m_startFlag && !m_stopFlag)
+	{
+		appearRateData.Ease(appearDistance, 30, Out);
+		fontPos = appearRateData.m_pos;
+	}
+	if (m_stopFlag)
+	{
+		disappearRateData.Ease(-1000.0f, 30, In);
+		fontPos = disappearRateData.m_pos;
+	}
+	fontPos.x = 550.0f;
+
+
+	if (m_startFlag || m_stopFlag)
+	{
+		++m_bigFontTimer;
+	}
+	if (30 <= m_bigFontTimer)
+	{
+		m_larpFontSize = { 2.0f,2.0f };
+		m_bigFontTimer = 0;
+	}
+	else
+	{
+		m_larpFontSize = { 1.0f,1.0f };
+	}
+	m_fontSize = KuroMath::Lerp(m_fontSize, m_larpFontSize, 0.2f);
+
+
 
 	m_pos += m_vel;
 	bool restartFlag = LIMIT_MAX_LINE <= m_pos.y;
@@ -31,9 +67,13 @@ void BonusEffect::Update()
 	{
 		m_pos.y = LIMIT_MIN_LINE;
 	}
-	if (restartFlag && m_stopFlag)
+	if (restartFlag && m_stopFlag && 1.0f <= disappearRateData.m_rate)
 	{
+		appearRateData.Init({ 0.0f, LIMIT_MAX_LINE });
+		disappearRateData.Init({ 0.0f, LIMIT_MAX_LINE + appearDistance });
+		m_bigFontTimer = 0;
 		m_startFlag = false;
+		m_stopFlag = false;
 	}
 }
 
@@ -44,6 +84,7 @@ void BonusEffect::Draw()
 		return;
 	}
 	DrawFunc2D::DrawRotaGraph2D(m_pos, m_size, 0.0f, m_feverEffectTexBuffer);
+	DrawFunc2D::DrawRotaGraph2D(fontPos, m_fontSize, 0.0f, m_bonusStageTexBuffer);
 }
 
 void BonusEffect::Start()
--- src/user/BonusEffect.h
+++ src/user/BonusEffect.h
@@ -15,10 +15,45 @@ public:
 private:
 	Vec2<float>m_pos, m_size, m_vel;
 	std::shared_ptr<TextureBuffer>m_feverEffectTexBuffer;
+	std::shared_ptr<TextureBuffer>m_bonusStageTexBuffer;
 
 	bool m_startFlag;
 	bool m_stopFlag;
 
 	const float LIMIT_MIN_LINE, LIMIT_MAX_LINE;
 	const int APPEAR_TIME;
+
+
+	struct EaseData
+	{
+		float m_rate;
+		Vec2<float> m_basePos;
+		Vec2<float> m_pos;
+
+		void Init(const Vec2<float> &BASE_POS)
+		{
+			m_rate = 0.0f;
+			m_basePos = BASE_POS;
+			m_pos = {};
+		};
+
+		void Ease(float DISTANCE, int TIMER, EASE_CHANGE_TYPE EASE)
+		{
+			if (m_rate < 1.0f)
+			{
+				m_rate += 1.0f / static_cast<float>(TIMER);
+			}
+			if (1.0f <= m_rate)
+			{
+				m_rate = 1.0f;
+			}
+			m_pos = m_basePos + KuroMath::Ease(EASE, Circ, m_rate, Vec2<float>(0.0f, 0.0f), Vec2<float>(1.0f, 1.0f)) * DISTANCE;
+		};
+	};
+
+	EaseData appearRateData;
+	EaseData disappearRateData;
+	Vec2<float>fontPos, m_fontSize, m_larpFontSize;
+	int m_bigFontTimer;
+	float appearDistance;
 };
