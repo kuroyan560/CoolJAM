1f8662c9d770dd2d9d453adf1359289902fde76f
hassy560 <k020g1167@g.neec.ac.jp>
2022-09-14 11:42:05 +0900

    ステージ床と外周円モデル分離。影フラグ別々に

--- src/user/StageFloor.h
+++ src/user/StageFloor.h
@@ -3,14 +3,17 @@
 #include<memory>
 #include"../engine/Common/Vec.h"
 #include"../engine/Common/Angle.h"
-class ModelObject;
+#include"Transform.h"
+class Model;
 class RenderTarget;
 
 class StageFloor : public Singleton<StageFloor>
 {
 	friend class Singleton<StageFloor>;
 
-	std::shared_ptr<ModelObject>m_modelObj;
+	std::shared_ptr<Model>m_floorModel;
+	std::shared_ptr<Model>m_circleModel;
+	Transform m_transform;
 	std::shared_ptr<RenderTarget>m_floorScreen;
 	Vec3<Angle>m_rotate;
 	StageFloor();
